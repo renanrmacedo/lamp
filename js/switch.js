00ab906a3c5a037b0958b9433080fa35efb17ca2
Renan Ribeiro Macedo <85801246+renanrmacedo@users.noreply.github.com>
2025-05-19 21:53:21 -0300

    adicionando javascript para ascender a lampada

---
+++ js/switch.js
@@ -0,0 +1,7 @@
+function turnLights() {
+    const lamp = document.getElementById("bulb");
+    const button = document.getElementById("switch");
+
+    lamp.classList.toggle('on');
+    button.classList.toggle('on');
+}
